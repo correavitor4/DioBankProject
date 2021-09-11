91361558d7fa1d33571a5afb75016a086b099d0e
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-09-11 20:22:09 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewDioBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
